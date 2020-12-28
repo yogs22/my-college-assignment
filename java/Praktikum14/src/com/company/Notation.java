4aac472441f6d9683bf0f8e708ec6d6caa3f538e
yogs22 <yogi.enjoyers22@gmail.com>
2020-12-28 19:12:09 +0700

    java: praktikum 14 - fix wrong calc

--- java/Praktikum14/src/com/company/Notation.java
+++ java/Praktikum14/src/com/company/Notation.java
@@ -4,15 +4,12 @@ import java.util.Scanner;
 public class Notation {
     public void calc(Integer data) {
         int value = 1;
-        if (data == 0 || data == 1) {
-            value = 1;
-        } else {
-            for (int i=1; i <= data; i++) {
-                value += i;
-            }
+        for (int i=2; i <= data; i++) {
+            value += i;
         }
+
         int total = value + (2 * value);
-        System.out.println("∑"+ data +",k=1(x+2i) = " + total);
+        System.out.println("∑"+data+",k=1(x+2i) = "+total);
     }
 
     public static void main(String[] args) {
