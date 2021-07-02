8a159e64d4e798ea38e0fbde35e8b36a8bed857f
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2021-07-02 22:05:23 +0700

    P13 - Praktikum OOP

--- 3_OOP/P13/src/com/company/DBConnection.java
+++ 3_OOP/P13/src/com/company/DBConnection.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.sql.*;
 
-public class Main {
+public class DBConnection {
     String url, usr, pwd, db;
     public DBConnection() {
         db = "javadb";
@@ -11,10 +11,31 @@ public class Main {
     }
 
     public Connection getConnection() {
-        
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, usr, pwd);
+            System.out.println("Ok : Driver Ditemukan");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println("Error: Driver Tidak Ditemukan \n"+e+"\n");
+        }
+        catch (SQLException e) {
+            System.out.println("Error: Tidak Dapat Connect ke Database \n"+e+"\n");
+        }
+
+        return con;
+    }
+
+    public void MakeDialog() {
+        System.out.println("Created by : Yogi Prasetyawan ~ yogi.web.id");
+        System.out.println("NIM : 191110236");
+        System.exit(0);
     }
 
     public static void main(String[] args) {
-
+        DBConnection dnd = new DBConnection();
+        dnd.getConnection();
+        dnd.MakeDialog();
     }
 }
