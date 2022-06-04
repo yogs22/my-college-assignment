61b3a7c0358441c22fb8b2dee692512e20ea7ede
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2022-06-04 07:13:46 +0700

    Create kerucut.cpp

---
+++ 5_KOMP_GRAFIK/kerucut.cpp
@@ -0,0 +1,55 @@
+#include <windows.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>
+#include <math.h> 
+#include <GL/gl.h>
+#include <GL/glut.h>
+#define PI 3.14
+
+GLint circle_points = 40;
+
+void MyCone(GLfloat radius, GLfloat tinggi) {
+	GLint i;
+	GLdouble theta, ntheta;
+	for	(i = 0;i < circle_points;i++) {
+		glBegin(GL_POLYGON);
+		theta = (2*PI*i/circle_points);
+		ntheta = (2*PI*(i+1)/circle_points);
+		glVertex3f(radius*cos(theta),0,radius*sin(theta));
+		glVertex3f(0,tinggi,0);
+		glVertex3f(radius*cos(ntheta),0,radius*sin(ntheta));
+		glEnd();
+	}
+}
+
+static void Display(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1,1,1);
+	MyCone(0.5,1);
+	glFlush();
+}
+
+void reshape (int w, int h){
+	glViewport(0,0, (GLsizei)w, (GLsizei) h);
+	glMatrixMode(GL_PROJECTION);
+	glFrustum(-(GLfloat)w/h,(GLfloat)w/h, -2,2,1,20);
+	gluLookAt(0,2,2,0,0,-100,0,1,0);
+}
+
+void init(void) {
+	glClearColor(0,0,0,0);
+	glPolygonMode(GL_FRONT, GL_LINE);
+	glPolygonMode(GL_BACK, GL_LINE);
+}
+
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(320, 200);
+	glutCreateWindow("Kerucut");
+	init();
+	glutDisplayFunc(Display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+}
