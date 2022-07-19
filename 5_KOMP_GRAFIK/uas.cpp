49708020dc3627788d1a3bcbd07457fc4f92c656
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2022-07-19 20:21:04 +0700

    Update uas.cpp

--- 5_KOMP_GRAFIK/uas.cpp
+++ 5_KOMP_GRAFIK/uas.cpp
@@ -104,7 +104,7 @@ int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(400, 350);
-	glutCreateWindow("Botol");
+	glutCreateWindow("Lampu UAS Yogi");
 	init();
 	glutDisplayFunc(Display);
 	glutReshapeFunc(reshape);
